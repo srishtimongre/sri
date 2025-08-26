d9eaf0c82d6a08b0835f49589b162f7d5169963a
srishti <srishtimongre9699@gmail.com>
2025-08-26 04:51:38 -0700

    percantage

---
+++ swap_3.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+print('before swapping:')
+print(a)
+print(b)
+print('after swapping:')
+c=a
+a=b
+b=c
+print(a)
+print(b)
+
