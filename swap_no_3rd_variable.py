e7e6cfcd63f1d1b755a396eca1838282142dc65c
srishti <srishtimongre9699@gmail.com>
2025-08-26 04:05:04 -0700

    Swapping Program

---
+++ swap_no_3rd_variable.py
@@ -0,0 +1,10 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========== RESTART: D:/python_programs/without using 3rd variable.py ==========
+enter the value a:10
+enter the value b:5
+before swapping
+after swapping:
+5
+10
