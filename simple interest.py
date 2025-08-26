04e7c671eeca83d2dcb95ec2d536837ccd6401c1
srishtimongre <srishtimongre9699@gmail.com>
2025-08-26 01:16:23 -0700

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,8 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================ RESTART: D:/python_programs/simple interest.py ================
+enter the value of principle:1000
+enter the value of rate:15
+enter the value of time:4
+the si is: 600.0
