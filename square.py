0e83541da75c4f025248015c4277dbc764fd2b45
srishti <srishtimongre9699@gmail.com>
2025-08-27 03:29:30 -0700

    square

---
+++ square.py
@@ -0,0 +1,11 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+====================== RESTART: D:\python_programs\rect.py =====================
+enter the value of base:12
+enter the value of height:4
+the area of triangle 24.0
+>>> 
+===================== RESTART: D:\python_programs\square.py ====================
+enter the value of side square:4
+area of square: 16
